5e5af78ab787c251788de582b3ad2dd8495709d1
Romesh Thavanathan <romeshthavanathan@gmail.com>
2020-12-17 22:08:38 -0330

    intro to React class components

---
+++ src/components/Loading.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+import classnames from "classnames";
+
+class Loading extends Component {
+  // constructor(props) {
+  //   super(props);
+
+  // }
+
+  render() {
+
+    return <Loading/>;
+  }
+}
+
+export default Loading;
---
+++ src/components/Panel.js
@@ -0,0 +1,27 @@
+import { trimCharsStart } from "lodash/fp";
+import React, { Component } from "react";
+
+
+class Panel extends Component {
+  // constructor(props) {
+  //   super(props);
+  //   };
+
+  
+
+  render() {
+    const { label, value , onSelect} = this.props;
+
+    return (
+      <section
+        className="dashboard__panel"
+        onClick={onSelect}
+      >
+        <h1 className="dashboard__panel-header">{label}</h1>
+        <p className="dashboard__panel-value">{value}</p>
+      </section>
+    );
+  }
+}
+
+export default Panel;
